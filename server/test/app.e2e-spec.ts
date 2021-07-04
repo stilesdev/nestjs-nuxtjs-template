87492b1aeb7dc099c69c2d6c0fd2039beae7bf0f
Matthew Stiles <mstiles92@gmail.com>
2021-07-04 00:52:21 -0400

    Use proper route for sample e2e test

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -15,7 +15,7 @@ describe('AppController (e2e)', () => {
         await app.init()
     })
 
-    it('/ (GET)', () => {
-        return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!')
+    it('/api (GET)', () => {
+        return request(app.getHttpServer()).get('/api').expect(200).expect('Hello World!')
     })
 })
